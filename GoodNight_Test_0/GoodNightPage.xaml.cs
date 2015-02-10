4d2e5968fc598eec004148bfd457cdbef15d2f59
neomax24 <neo_max24@hotmail.com>
2015-02-11 02:46:21 +0800

    check list test

--- GoodNight_Test_0/GoodNightPage.xaml.cs
+++ GoodNight_Test_0/GoodNightPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -22,9 +23,21 @@ namespace GoodNight_Test_0
     /// </summary>
     public sealed partial class GoodNightPage : Page
     {
+        public class data_checklist_time_peroid_struct
+        {
+            public string name { get; set; }
+            public int time_peroid { get; set; }
+
+            public string time_peroid_string { get; set; }
+            public bool is_work { get; set; }
+
+        }
+        List<data_checklist_time_peroid_struct> data_checklist_time_peroid=new List<data_checklist_time_peroid_struct>();
+
         public GoodNightPage()
         {
             this.InitializeComponent();
+
         }
 
         /// <summary>
@@ -34,10 +47,26 @@ namespace GoodNight_Test_0
         /// 此参数通常用于配置页。</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-
+            ///Bling测试
+            
+            for (int i = 0; i < 10; i++)
+            {
+                data_checklist_time_peroid_struct data_checklist_time_peroid_struct_item = new data_checklist_time_peroid_struct();
+                data_checklist_time_peroid_struct_item.name = "test" + i.ToString();
+                data_checklist_time_peroid_struct_item.time_peroid = i;
+                data_checklist_time_peroid_struct_item.time_peroid_string = i.ToString();
+                data_checklist_time_peroid_struct_item.is_work = true;
+                data_checklist_time_peroid.Add(data_checklist_time_peroid_struct_item);
+            }
+            checklist_time_peroid.ItemsSource = data_checklist_time_peroid;
         }
 
         private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListPickerFlyout_ItemsPicked(ListPickerFlyout sender, ItemsPickedEventArgs args)
         {
 
         }
